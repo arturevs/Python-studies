467e26ad90995cbab1536cb99301b2ac6f3b5cf2
artur <artur.rp2001@gmail.com>
2023-01-24 00:07:16 -0300

    fixed code

--- calc_IMC.py
+++ calc_IMC.py
@@ -1,19 +1,9 @@
 
-def swap_char(string1, char1, char2):
-  string2 = []
-  contador = 0
-  for i in string1:
-    if i == char1:
-      string2[contador] = char2
-      contador += 1
-    else:
-      string2[contador] = i
-      contador += 1
-    return string(string2)
 
-nome = input("Digite seu nome: ")
-peso = float(input("Digite seu peso: "))
-altura = input("Digite sua altura em metros: ")
-altura = swap_char(altura, ',', '.')
-altura = float(altura)
-print(f'{nome} tem {altura} metros de altura, pesa {peso} quilos e seu IMC é {IMC:.2f}')
+
+name = input("Your name: ")
+weight = float(input("Your weight in kilos: "))
+height = input("Your height in meters, like 1.75: ")
+height = float(height)
+IMC = weight / height**2
+print(f'{name} is {height} meters tall, weights {weight} kilos and their IMC is {IMC:.2f}')
